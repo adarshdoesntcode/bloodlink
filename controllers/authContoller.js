ee6a6d397a2f81c1fb6fa0a3e4922761629f7678
Adarsh Das <adarsh.191605@ncit.edu.np>
2023-09-21 11:06:18 +0545

    updatedauth

--- controllers/authContoller.js
+++ controllers/authContoller.js
@@ -47,7 +47,7 @@ exports.signup = async(req, res) => {
             password: req.body.password,
             address: req.body.address,
             longitude: req.body.longitude,
-            latitude:req.body.latitude
+            latitude: req.body.latitude
         });
 
         //sending the response only
@@ -230,3 +230,33 @@ exports.isLoggedIn = async(req, res, next) => {
     }
     next();
 };
+
+
+
+exports.auth = async(req, res, next) => {
+    try {
+        const token = req.header('x-auth-token');
+        console.log('inside auth middle ware')
+        console.log(`token ${token}`);
+
+        if (!token) {
+            return res.status(401).json({
+                message: "No auth token, access denied",
+            });
+        }
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        if (!verified) return res.status(401).json({
+            message: "Token verification failed, authorization denied",
+        })
+
+        req.user = verified.id;
+        req.token = token;
+        // console.log(`req.user:${req.user}`);
+        // console.log(`req.token:${req.token}`);
+        next(); //call next callback function
+
+    } catch (error) {
+
+    }
+}
